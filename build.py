828020fbad2590a6cea01ffff96ea4eb8c6860ff
kaixin1106 <122666772+kaixin168sxz@users.noreply.github.com>
2026-05-31 19:51:02 +0800

    fix: mkdir if cannot find dir/

--- build.py
+++ build.py
@@ -1,11 +1,15 @@
 from jinja2 import Environment, FileSystemLoader
-import os
+from pathlib import Path
 
-OUTPUT_DIR = "dist"
+root_path = Path.cwd()
+OUTPUT_DIR = root_path/"dist"
 
 env = Environment(loader=FileSystemLoader("templates"))
 template = env.get_template("index.html")
 index_html = template.render()
 
-with open(os.path.join(OUTPUT_DIR, "index.html"), 'w', encoding='utf-8') as f:
-        f.write(index_html)
+if not OUTPUT_DIR.exists():
+    OUTPUT_DIR.mkdir()
+
+with open(OUTPUT_DIR/"index.html", 'w', encoding='utf-8') as f:
+    f.write(index_html)
